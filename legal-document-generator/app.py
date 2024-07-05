ae9f47b24025c4d2a0cb27991b7acb41af9a9718
akaasif <khanasif1@gmail.com>
2024-07-05 23:26:57 +1000

    summary working

--- legal-document-generator/app.py
+++ legal-document-generator/app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from document_generator import generate_doc, generate_pdf
+from document_generator import generate_doc, generate_pdf,summarize_read_docx,summarize_process_file_content
+from docx import Document
 import tempfile
 import json
+
 # Title displayed on the Streamlit Web App
 st.set_page_config(page_title="Document Generator", page_icon=":tada", layout="wide")
 
@@ -24,8 +26,10 @@ with st.container():
 
 # Saving LLM response as variable
 temp_llm_response = ""
+# Create two columns
 
 # Create Buttons and start document generation workflow upon "Submit"
+
 result = st.button("Generate Document")
 # if a result is created by the LLM...
 if result:
@@ -39,11 +43,9 @@ if result:
     # try:
     # Load and parse JSON input
     print('start pdf create')
-        
-    # print(llm_response)
 
     # Generate PDF
-    pdf = generate_pdf(llm_response['content'][0]['text'])
+    pdf = generate_pdf(llm_response['content'][0]['text'],1)
 
     pdf_output = 'Consultancy_Services_Agreement.pdf'
     pdf.output(pdf_output)
@@ -55,6 +57,32 @@ if result:
     # except Exception as e:
     #     st.error(f'Error generating PDF: {e}')    
 
+# add a line of spacing in the front end app
+st.write("---")
+
+with st.container():
+    # File uploader
+    uploaded_file = st.file_uploader("Choose a DOCX file", type="docx")
+    if uploaded_file is not None:
+        # Read the file content
+        content = summarize_read_docx(uploaded_file).lstrip()
+        # Display the content (optional)
+        st.text_area("File Content", content, height=200)
+    summarize = st.button("Summarize Document")
+    if summarize:
+        print("Summarize result")
+         # Pass the content to the function
+        llm_response = summarize_process_file_content(content)
+        st.markdown(llm_response['content'][0]['text'])
+        # Generate PDF
+        pdf = generate_pdf(llm_response['content'][0]['text'],2)
+
+        pdf_output = 'Summary_Consultancy_Services_Agreement.pdf'
+        pdf.output(pdf_output)
+
+        # Provide download link
+        with open(pdf_output, 'rb') as f:
+            st.download_button('Download PDF', f, file_name=pdf_output)
 
 # add a line of spacing in the front end app
 st.write("---")
--- legal-document-generator/document_generator.py
+++ legal-document-generator/document_generator.py
@@ -4,7 +4,7 @@ import json
 import botocore.config
 from dotenv import load_dotenv
 from fpdf import FPDF
-
+from docx import Document
 # loading environment variables
 load_dotenv()
 # configure Bedrock client
@@ -35,30 +35,41 @@ def create_pdf(details, content):
     print("**** In Create PDF*****")
     pdf = PDF()
     pdf.add_page()
-    pdf.add_table(details)
+    if(details != ""):
+        pdf.add_table(details)
     pdf.add_content(content)
     return pdf
 
 # Function to generate PDF from JSON content
-def generate_pdf(data_text):
+def generate_pdf(data_text, content_type):
     print("**** In Generate PDF*****")
-    details = {}
-    # data_text1 = "# Consultancy Services Agreement\n\n| Principal | Revantage <br> Revantage, Suite 612, Level 2, 150 Castlereagh Street, Melb Vic 2000 |\n|------------|----------------------------------------------------------------------------------|\n| Consultant | John Boggs <br> ABN: Not eligible <br> Address: 1 Bogg Drive, Bogg City, Australia |\n| Commencement Date | 25 March 2026 |\n| Term | As per General Terms clause 1 |\n| Services | As agreed between the parties |\n| Nominated Person | John Boggs |\n| Time Commitment | As required to perform the Services |\n| Principal Representative | As nominated by the Principal from time to time |\n| Required Licences | As required to legally perform the Services |\n| Service Fee | $10,000 |\n| Payment Terms | 14 days from receipt of invoice |\n| Required Insurance | Public liability insurance: $10,000,000 to $90,000,000 <br> Professional indemnity insurance: As required for the Services <br> Workers compensation insurance: As required by law |\n| Required Notice | 1 month |\n\nDated: [Agreement date]\n\nEXECUTED by Revantage by its attorneys under power of attorney dated [date]:\n\n..............................................................\nAttorney  \n\n..............................................................\nName of Attorney (print)\n\n..............................................................\nAttorney\n\n..............................................................  \nName of Attorney (print)  \n\n\nEXECUTED by John Boggs in accordance with section 127(1) of the Corporations Act 2001 (Cth):\n\n..............................................................\nSole Company Secretary and Director\n\n..............................................................\nName of Sole Company Secretary and Director (print)\n\n\n## GENERAL TERMS  \n\n1. **TERM**\n    - **Initial Term**: The Consultant will provide the Services for the Initial Term commencing on the Commencement Date.\n    - **Further Term**: The Principal may, not less than 14 days before the expiry of the Initial Term, elect by written notice to the Consultant to extend the Agreement for a Further Term. The Consultant must inform the Principal in writing of their consent or otherwise to the extension. If agreed, the Consultant will continue to provide the Services under the terms of this Agreement for the Further Term. If not agreed, the Agreement will continue on a month-to-month basis until terminated by either party in accordance with the termination clause.\n\n2. **SERVICES**\n    - **Consultant to Perform Services**: The Principal engages the Consultant to perform the Services. \n    - **Services for Benefit of the Group**: The Consultant will provide the Services for the benefit of the Principal and other companies in the Group or clients of the Principal as directed by the Principal.\n    - **Service Standards**: The Consultant must ensure the Services are provided in accordance with reasonable directions, with due care, skill, and diligence, in compliance with all applicable laws, professional standards, and Revantage's policies and procedures.\n    - **Location of Services**: The Consultant may determine the location from which the Services are performed.\n\n3. **NATURE OF RELATIONSHIP**\n    - **Principal and Independent Consultant**: The Consultant is engaged as an independent non-exclusive contractor and is not an employee of the Principal or any company in the Group.\n    - **No Authority**: The Consultant is not authorized to act on behalf of any company in the Group without the Principal's prior consent.\n\n4. **FEES**\n    - **Service Fee**: Provided the Services are delivered in accordance with this Agreement, the Principal will pay the Consultant the Service Fee within the required timeframe.\n    - **Additional Services**: If additional services are requested and agreed in writing, the Principal will pay the Consultant for the additional services at a rate agreed in writing, exclusive of GST.\n    - **Invoice**: The Consultant must issue an invoice for payment for Services performed on a monthly basis.\n\n5. **EXPENSES**\n    - The Consultant will provide the Services at its own cost and, unless expressly authorized by the Principal, will not be entitled to reimbursement for any out-of-pocket expenses.\n\n6. **EQUIPMENT**\n    - The Consultant must provide all necessary equipment at its expense, including a laptop and mobile phone.  \n\n7. **CONFIDENTIALITY**\n    - **Obligations**: The Consultant must protect the confidential nature of the Confidential Information and not disclose or use it without the Principal's prior written consent, unless required by law.\n    - **Return of Property**: On termination of the Agreement or upon request, the Consultant must return all documents and property of the Principal or the Group.\n\n8. **INTELLECTUAL PROPERTY**\n    - **Ownership**: All Intellectual Property Rights in Contract Materials are owned exclusively by the Principal. The Consultant assigns all such rights to the Principal.\n    - **Third-Party Rights**: The Consultant must not infringe any third-party Intellectual Property Rights in performing the Services and indemnifies the Principal against any claims.\n\n9. **RESPONSIBILITIES OF CONSULTANT**\n    - **Insurance**: The Consultant must maintain the required insurances and provide certificates of currency upon request.\n    - **No Conflict**: The Consultant must not enter into agreements that may conflict with their obligations under this Agreement. \n    - **Indemnity**: The Consultant indemnifies the Principal against liabilities arising from the provision of the Services.\n\n10. **TERMINATION**\n    - This Agreement may be terminated by either party with the Required Notice or immediately for a breach that has not been remedied within 21 days of notice or in cases of bankruptcy or insolvency.\n\n11. **ASSIGNMENT**\n    - The Consultant may not assign their rights under this Agreement without the Principal's written consent. The Principal may assign or subcontract its rights without the Consultant's consent.\n\n12. **PRIVACY AND DATA BREACH**\n    - The Consultant must maintain adequate data protection procedures and notify the Principal immediately of any data breaches, cooperating fully in any investigations.\n\n13. **WORKPLACE HEALTH & SAFETY**\n    - The Consultant must ensure compliance with all applicable workplace health and safety legislation and standards.\n\n14. **OFFICE OF FOREIGN ASSET CONTROL**\n    - The Consultant represents compliance with all anti-terrorism and anti-money laundering regulations and will provide necessary information for the Principal to ensure compliance.\n\n15. **NON-DISCLOSURE OF PRINCIPAL NAME**\n    - The Consultant shall not use the Principal trademarks or name in external publicity without prior written consent.\n\n16. **ANTI-CORRUPTION COMPLIANCE**\n    - The Consultant represents that no payments have been or will be made in violation of anti-corruption laws and agrees to inform the Principal of any violations.\n\n17. **ANTI-MONEY LAUNDERING COMPLIANCE**\n    - The Consultant will comply with all applicable money laundering laws and inform the Principal of any violations.\n\n18. **SANCTIONS**\n    - The Consultant is not subject to any sanctions administered or enforced by relevant authorities and will not knowingly allow a Prohibited Entity to obtain an interest in the Agreement.\n\n19. **GST**\n    - All consideration under this Agreement is exclusive of GST. If GST is payable on any supply, the recipient must pay the amount equal to the GST payable."
-    print(data_text)
-    lines = data_text.split('\n')
-    print(len(lines))
-    for line in lines:
-        if line.startswith('| '):
-            key_value = line.split(' | ')
-            if len(key_value) == 2:
-                key = key_value[0].strip().replace('|', '')
-                value = key_value[1].strip().replace('|', '').replace('<br>', '\n')
-                details[key] = value
-                print(f'Key:{key} , value:{value}')
+    responde_pdf = PDF()
+    if(content_type == 1):
+        print('Generate AI content pdf')
+        details = {}
+        # data_text1 = "# Consultancy Services Agreement\n\n| Principal | Revantage <br> Revantage, Suite 612, Level 2, 150 Castlereagh Street, Melb Vic 2000 |\n|------------|----------------------------------------------------------------------------------|\n| Consultant | John Boggs <br> ABN: Not eligible <br> Address: 1 Bogg Drive, Bogg City, Australia |\n| Commencement Date | 25 March 2026 |\n| Term | As per General Terms clause 1 |\n| Services | As agreed between the parties |\n| Nominated Person | John Boggs |\n| Time Commitment | As required to perform the Services |\n| Principal Representative | As nominated by the Principal from time to time |\n| Required Licences | As required to legally perform the Services |\n| Service Fee | $10,000 |\n| Payment Terms | 14 days from receipt of invoice |\n| Required Insurance | Public liability insurance: $10,000,000 to $90,000,000 <br> Professional indemnity insurance: As required for the Services <br> Workers compensation insurance: As required by law |\n| Required Notice | 1 month |\n\nDated: [Agreement date]\n\nEXECUTED by Revantage by its attorneys under power of attorney dated [date]:\n\n..............................................................\nAttorney  \n\n..............................................................\nName of Attorney (print)\n\n..............................................................\nAttorney\n\n..............................................................  \nName of Attorney (print)  \n\n\nEXECUTED by John Boggs in accordance with section 127(1) of the Corporations Act 2001 (Cth):\n\n..............................................................\nSole Company Secretary and Director\n\n..............................................................\nName of Sole Company Secretary and Director (print)\n\n\n## GENERAL TERMS  \n\n1. **TERM**\n    - **Initial Term**: The Consultant will provide the Services for the Initial Term commencing on the Commencement Date.\n    - **Further Term**: The Principal may, not less than 14 days before the expiry of the Initial Term, elect by written notice to the Consultant to extend the Agreement for a Further Term. The Consultant must inform the Principal in writing of their consent or otherwise to the extension. If agreed, the Consultant will continue to provide the Services under the terms of this Agreement for the Further Term. If not agreed, the Agreement will continue on a month-to-month basis until terminated by either party in accordance with the termination clause.\n\n2. **SERVICES**\n    - **Consultant to Perform Services**: The Principal engages the Consultant to perform the Services. \n    - **Services for Benefit of the Group**: The Consultant will provide the Services for the benefit of the Principal and other companies in the Group or clients of the Principal as directed by the Principal.\n    - **Service Standards**: The Consultant must ensure the Services are provided in accordance with reasonable directions, with due care, skill, and diligence, in compliance with all applicable laws, professional standards, and Revantage's policies and procedures.\n    - **Location of Services**: The Consultant may determine the location from which the Services are performed.\n\n3. **NATURE OF RELATIONSHIP**\n    - **Principal and Independent Consultant**: The Consultant is engaged as an independent non-exclusive contractor and is not an employee of the Principal or any company in the Group.\n    - **No Authority**: The Consultant is not authorized to act on behalf of any company in the Group without the Principal's prior consent.\n\n4. **FEES**\n    - **Service Fee**: Provided the Services are delivered in accordance with this Agreement, the Principal will pay the Consultant the Service Fee within the required timeframe.\n    - **Additional Services**: If additional services are requested and agreed in writing, the Principal will pay the Consultant for the additional services at a rate agreed in writing, exclusive of GST.\n    - **Invoice**: The Consultant must issue an invoice for payment for Services performed on a monthly basis.\n\n5. **EXPENSES**\n    - The Consultant will provide the Services at its own cost and, unless expressly authorized by the Principal, will not be entitled to reimbursement for any out-of-pocket expenses.\n\n6. **EQUIPMENT**\n    - The Consultant must provide all necessary equipment at its expense, including a laptop and mobile phone.  \n\n7. **CONFIDENTIALITY**\n    - **Obligations**: The Consultant must protect the confidential nature of the Confidential Information and not disclose or use it without the Principal's prior written consent, unless required by law.\n    - **Return of Property**: On termination of the Agreement or upon request, the Consultant must return all documents and property of the Principal or the Group.\n\n8. **INTELLECTUAL PROPERTY**\n    - **Ownership**: All Intellectual Property Rights in Contract Materials are owned exclusively by the Principal. The Consultant assigns all such rights to the Principal.\n    - **Third-Party Rights**: The Consultant must not infringe any third-party Intellectual Property Rights in performing the Services and indemnifies the Principal against any claims.\n\n9. **RESPONSIBILITIES OF CONSULTANT**\n    - **Insurance**: The Consultant must maintain the required insurances and provide certificates of currency upon request.\n    - **No Conflict**: The Consultant must not enter into agreements that may conflict with their obligations under this Agreement. \n    - **Indemnity**: The Consultant indemnifies the Principal against liabilities arising from the provision of the Services.\n\n10. **TERMINATION**\n    - This Agreement may be terminated by either party with the Required Notice or immediately for a breach that has not been remedied within 21 days of notice or in cases of bankruptcy or insolvency.\n\n11. **ASSIGNMENT**\n    - The Consultant may not assign their rights under this Agreement without the Principal's written consent. The Principal may assign or subcontract its rights without the Consultant's consent.\n\n12. **PRIVACY AND DATA BREACH**\n    - The Consultant must maintain adequate data protection procedures and notify the Principal immediately of any data breaches, cooperating fully in any investigations.\n\n13. **WORKPLACE HEALTH & SAFETY**\n    - The Consultant must ensure compliance with all applicable workplace health and safety legislation and standards.\n\n14. **OFFICE OF FOREIGN ASSET CONTROL**\n    - The Consultant represents compliance with all anti-terrorism and anti-money laundering regulations and will provide necessary information for the Principal to ensure compliance.\n\n15. **NON-DISCLOSURE OF PRINCIPAL NAME**\n    - The Consultant shall not use the Principal trademarks or name in external publicity without prior written consent.\n\n16. **ANTI-CORRUPTION COMPLIANCE**\n    - The Consultant represents that no payments have been or will be made in violation of anti-corruption laws and agrees to inform the Principal of any violations.\n\n17. **ANTI-MONEY LAUNDERING COMPLIANCE**\n    - The Consultant will comply with all applicable money laundering laws and inform the Principal of any violations.\n\n18. **SANCTIONS**\n    - The Consultant is not subject to any sanctions administered or enforced by relevant authorities and will not knowingly allow a Prohibited Entity to obtain an interest in the Agreement.\n\n19. **GST**\n    - All consideration under this Agreement is exclusive of GST. If GST is payable on any supply, the recipient must pay the amount equal to the GST payable."
+        print(data_text)
+        lines = data_text.split('\n')
+        print(len(lines))
+        for line in lines:
+            if line.startswith('| '):
+                key_value = line.split(' | ')
+                if len(key_value) == 2:
+                    key = key_value[0].strip().replace('|', '')
+                    value = key_value[1].strip().replace('|', '').replace('<br>', '\n')
+                    details[key] = value
+                    print(f'Key:{key} , value:{value}')
+        
+        rest_of_content = '\n'.join(lines[16:])
+        rest_of_content =  rest_of_content.replace('##','').replace('**','')
+        responde_pdf = create_pdf(details,rest_of_content)
+    else:
+        print('Generate summary pdf')
+        data_text = data_text.replace('#','').replace('**','')
+        responde_pdf = create_pdf("",data_text)
+    return responde_pdf
+
     
-    rest_of_content = '\n'.join(lines[16:])
-    rest_of_content =  rest_of_content.replace('##','').replace('**','')
-    return create_pdf(details,rest_of_content)
 
 def invoke_llm(bedrock, user_input, doc_template) -> str:
     """
@@ -381,4 +392,38 @@ def generate_doc(user_input) -> str:
     # call the invoke_llm_refine function to generate the refined draft of the document that the user is trying to create
     llmOutput = invoke_llm_refine(bedrock,user_refine, llm_output, doc_template)
     # return the refined version of the document create by the invoke_llm_refine function
+    return llmOutput
+
+def summarize_read_docx(file):
+    doc = Document(file)
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return '\n'.join(full_text)
+
+def summarize_process_file_content(content):
+    prompt = "Generate summary for the content, creat ten bullet point summary"
+    user_input = "\n".join([prompt, content])
+    # print(user_input)
+    doc_template = """
+        [Document Title]
+
+        [Provide the main heading or title of the document here]
+        ## Quick Summary
+        [Provide a brief summary or abstract of the document's content here. This should be a short paragraph that gives an overview of the main points or purpose of the document.]
+
+        ## Summarized Bullet Points
+        - **Key Point 1**: [Provide a concise description of the first key point here]
+        - **Key Point 2**: [Provide a concise description of the second key point here]
+        - **Key Point 3**: [Provide a concise description of the third key point here]
+        - **Key Point 4**: [Provide a concise description of the fourth key point here]
+        - **Key Point 5**: [Provide a concise description of the fifth key point here]
+        …
+        - **Key Point X**: [Provide a concise description of the fifth key point here]
+    
+    """
+    # call the invoke_llm function to generate the first draft of the document the user is trying to create
+    llmOutput = invoke_llm(bedrock, user_input, doc_template)
+    # return the first draft of the document created by the invoke_llm function
+    print(f'************LLM Output :************ \n {llmOutput}')
     return llmOutput
